a4bf1bbf11cd1b0b37b0f30deca3415d8143eb16
Scott Sickles <scottsickles@gmail.com>
2023-12-13 11:42:39 -0500

    added variables for access token authentication

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "web_client_id" {
   value       = var.web_client_id
   description = "string || The web client id of the user pool."
 }
+
+output "access_key_id" {
+  value       = var.access_key_id
+  description = "string || The access key id to be used to access this user pool."
+}
+
+output "secret_access_key" {
+  value       = var.secret_access_key
+  description = "string || The secret access key to be used to access this user pool."
+}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,25 @@ variable "web_client_id" {
   type        = string
   description = "The web id of the existing user pool client. e.g. abcdefabcdef1234567890123"
 }
+
+variable "access_key_id" {
+  type        = string
+  default     = ""
+  sensitive   = true
+  description = <<EOF
+The AWS Access Key Id for accessing this user pool.
+Provide a value for this variable if the user pool is in another AWS account.
+Leave this blank if the user pool is in this same AWS account.
+EOF
+}
+
+variable "secret_access_key" {
+  type        = string
+  default     = ""
+  sensitive   = true
+  description = <<EOF
+The AWS Secret Access Key for accessing this user pool.
+Provide a value for this variable if the user pool is in another AWS account.
+Leave this blank if the user pool is in this same AWS account.
+EOF
+}

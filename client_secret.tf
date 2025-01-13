c8456fda1541ad4145f9fc529461201b1c76dc37
Scott Sickles <scottsickles@gmail.com>
2025-01-13 17:08:33 -0500

    if a client_secret is not provided, dont try to create a secret and return a blank value for client_secret_secret_id

--- client_secret.tf
+++ client_secret.tf
@@ -1,9 +1,13 @@
 resource "aws_secretsmanager_secret" "client_secret" {
+  count = var.client_secret != "" ? 1 : 0
+
   name = "${local.resource_name}/client_secret"
   tags = local.tags
 }
 
 resource "aws_secretsmanager_secret_version" "client_secret" {
+  count         = var.client_secret != "" ? 1 : 0
+
   secret_id     = aws_secretsmanager_secret.client_secret.id
   secret_string = var.client_secret
 }
